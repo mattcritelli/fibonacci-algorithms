69c831b0509d0b52f80883afd4654313cbcbee78
mattcritelli <matthew.critelli@gmail.com>
2017-03-07 12:32:48 -0700

    Add rspec tests for recursive solution and pass all tests

--- fibonacci.rb
+++ fibonacci.rb
@@ -10,4 +10,8 @@ def iterative_nth_fibonacci_number(n)
 end
 
 def recursive_nth_fibonacci_number(n)
+  return 0 if n == 0
+  return 1 if n == 1
+
+  recursive_nth_fibonacci_number(n-1) + recursive_nth_fibonacci_number(n-2)
 end
